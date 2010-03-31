a56908d75eef04c10c1350ab03cd69c494db5bc8
Meteor Liu <meteor1113@gmail.com>
2010-03-31 17:05:28 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- cpp/libutil/test/testalgo.cpp
+++ cpp/libutil/test/testalgo.cpp
@@ -11,59 +11,75 @@ void Test()
     {
         int i = 0;
         double d = 0.0;
-        assert(algo::LexicalCast(i, 12345));
+        bool ret = algo::LexicalCast(i, 12345);
+        assert(ret);
+        assert(i == 12345);
         std::cout << "12345 ->" << i << "(true)" << std::endl;
-        assert(algo::LexicalCast(d, 12345));
+        ret = algo::LexicalCast(d, 12345);
+        assert(ret);
+        // assert(d == 12345);
         std::cout << "12345 ->" << d << "(true)" << std::endl;
-        assert(algo::LexicalCast(d, 0.12345));
-        std::cout << "0.12345 ->" << d << "(true)" << std::endl;
-        assert(!algo::LexicalCast(i, 1.12345));
-        std::cout << "1.12345 ->" << i << "(false)" << std::endl;
+        ret = algo::LexicalCast(d, 0.12345);
+        assert(ret);
+        // assert(d == 0.12345);
+        std::cout << "0.12345 -> " << d << "(true)" << std::endl;
+        ret = algo::LexicalCast(i, 1.12345);
+        assert(!ret);
+        // assert(i == 1);
+        std::cout << "1.12345 -> " << i << "(false)" << std::endl;
     }
 
     std::cout << std::endl << "StringToNumber----------------:" << std::endl;
     {
         int i = 0;
         double d = 0.0;
-        assert(algo::StringToNumber(i, "12345"));
-        std::cout << "12345 ->" << i << "(true)" << std::endl;
-        assert(!algo::StringToNumber(i, "a12345"));
-        std::cout << "a12345 ->" << i << "(false)" << std::endl;
-        assert(!algo::StringToNumber(i, "123a45"));
-        std::cout << "123a45 ->" << i << "(false)" << std::endl;
-        assert(!algo::StringToNumber(i, "12345a"));
-        std::cout << "12345a ->" << i << "(false)" << std::endl;
+        bool ret = algo::StringToNumber(i, "12345");
+        assert(ret);
+        assert(i == 12345);
+        std::cout << "12345 -> " << i << "(true)" << std::endl;
+        ret = algo::StringToNumber(i, "a12345");
+        assert(!ret);
+        std::cout << "a12345 -> " << i << "(false)" << std::endl;
+        ret = algo::StringToNumber(i, "123a45");
+        assert(!ret);
+        std::cout << "123a45 -> " << i << "(false)" << std::endl;
+        ret = algo::StringToNumber(i, "12345a");
+        assert(!ret);
+        std::cout << "12345a -> " << i << "(false)" << std::endl;
 
-        assert(algo::StringToNumber(d, "0.12345"));
-        std::cout << "0.12345 ->" << d << "(true)" << std::endl;
-        assert(!algo::StringToNumber(d, "a0.12345"));
-        std::cout << "a0.12345 ->" << d << "(false)" << std::endl;
-        assert(!algo::StringToNumber(d, "a0.12a345"));
-        std::cout << "0.12a345 ->" << d << "(false)" << std::endl;
-        assert(!algo::StringToNumber(d, "0.12345a"));
-        std::cout << "0.12345a ->" << d << "(false)" << std::endl;
+        ret = algo::StringToNumber(d, "0.12345");
+        assert(ret);
+        // assert(d == 0.12345);
+        std::cout << "0.12345 -> " << d << "(true)" << std::endl;
+        ret = algo::StringToNumber(d, "a0.12345");
+        assert(!ret);
+        std::cout << "a0.12345 -> " << d << "(false)" << std::endl;
+        ret = algo::StringToNumber(d, "a0.12a345");
+        assert(!ret);
+        std::cout << "0.12a345 -> " << d << "(false)" << std::endl;
+        ret = algo::StringToNumber(d, "0.12345a");
+        assert(!ret);
+        std::cout << "0.12345a -> " << d << "(false)" << std::endl;
 
-        assert(algo::StringToNumber(d, "12345"));
-        std::cout << "12345 ->" << d << "(true)" << std::endl;
+        ret = algo::StringToNumber(d, "12345");
+        assert(ret);
+        // assert(d == 12345);
+        std::cout << "12345 -> " << d << "(true)" << std::endl;
 
-        assert(algo::StringToNumber(i, "0x12345a", std::hex));
-        std::cout << "0x12345a ->" << i << "(true)" << std::endl;
-        assert(!algo::StringToNumber(i, "0x12345a"));
-        std::cout << "0x12345a ->" << i << "(false)" << std::endl;
-        assert(!algo::StringToNumber(i, "0x12345g"));
-        std::cout << "0x12345g ->" << i << "(false)" << std::endl;
+        ret = algo::StringToNumber(i, "0x12345a", std::hex);
+        assert(ret);
+        assert(i == 0x12345a);
+        std::cout << "0x12345a -> " << i << "(true)" << std::endl;
+        ret = algo::StringToNumber(i, "0x12345a");
+        assert(!ret);
+        std::cout << "0x12345a -> " << i << "(false)" << std::endl;
+        ret = algo::StringToNumber(i, "0x12345g");
+        assert(!ret);
+        std::cout << "0x12345g -> " << i << "(false)" << std::endl;
     }
-
-#ifdef _DEBUG
-    std::cout << "_DEBUG" << std::endl;
-#endif
-#ifdef NDEBUG
-    std::cout << "NDEBUG" << std::endl;
-#endif
-
 }
-        
-        
+
+
 int main()
 {
     Test();
